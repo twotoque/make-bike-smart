a67b6ac20587367eb4ceadc047c5ce0bb8803e3f
Derek Song <dereksong28@gmail.com>
2025-12-28 17:19:43 -0500

    Added running data

--- ml-model/getdata.py
+++ ml-model/getdata.py
@@ -37,7 +37,7 @@ def fetch_all_activities(access_token):
         
     return pd.DataFrame(activities)
 
-def fetch_biking_streams(rides_df, access_token):
+def fetch_streams(rides_df, access_token, flle_path):
     """
     Loops through all biking activities and fetches second-by-second 
     heart rate and time data for ML training.
@@ -76,7 +76,7 @@ def fetch_biking_streams(rides_df, access_token):
 
     if all_stream_data:
         final_df = pd.concat(all_stream_data, ignore_index=True)
-        final_df.to_csv('ml-model/biking_time_series.csv', index=False)
+        final_df.to_csv(flle_path, index=False)
         return final_df
     
     return pd.DataFrame()
@@ -96,7 +96,17 @@ if __name__ == "__main__":
         rides_df.to_csv('ml-model/all_strava_bike_data.csv', index=False)
         print(f"Exported {len(rides_df)} ride summaries to 'ml-model/all_strava_bike_data.csv'")
 
-        biking_time_series = fetch_biking_streams(rides_df, token)
+
+
+        runs_df = all_activities_df[all_activities_df['type'] == 'Run']
+        running_time_series = fetch_streams(runs_df, token, "ml-model/running_time_series.csv")
+        if not running_time_series.empty:
+            print(f"Success! Exported {len(running_time_series)} data points to 'ml-model/running_time_series.csv'")
+        else:
+            print("No time-series data found for the identified runs.")
+
+
+        biking_time_series = fetch_streams(rides_df, token, "ml-model/biking_time_series.csv")
         
         if not biking_time_series.empty:
             print(f"Success! Exported {len(biking_time_series)} data points to 'ml-model/biking_time_series.csv'")
