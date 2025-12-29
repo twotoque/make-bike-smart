73d3d42b2b5c1b6923e297fa459ce33cd2081260
Derek Song <dereksong28@gmail.com>
2025-12-28 21:26:07 -0500

    xcode swift setup

---
+++ Make Bike Smart/Make Bike Smart/Make_Bike_SmartApp.swift
@@ -0,0 +1,17 @@
+//
+//  Make_Bike_SmartApp.swift
+//  Make Bike Smart
+//
+//  Created by Derek Song on 2025-12-28.
+//
+
+import SwiftUI
+
+@main
+struct Make_Bike_SmartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
